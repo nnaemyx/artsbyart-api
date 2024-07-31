c88a3ecab5f5b28f358664c85b7c07d662159991
johnpaul <edehjohnpaul@gmail.com>
2024-07-31 12:28:01 +0100

    new

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+})
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
